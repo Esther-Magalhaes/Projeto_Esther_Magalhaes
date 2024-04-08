1073c89a3652a6c16a9aea25ebc354168cf377c8
Esther-Magalhaes <esthermags21@gmail.com>
2024-04-08 04:50:50 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,28 @@
+public class C {
+    private String C1;
+    private Integer C2;
+
+    public String getC1() {
+        return C1;
+    }
+
+    public void setC1(String C1) {
+        this.C1 = C1;
+    }
+
+    public Integer getC2() {
+        return C2;
+    }
+
+    public void setC2(Integer C2) {
+        this.C2 = C2;
+    }
+
+    public void MC1() {
+        System.out.println("Método MC1");
+    }
+
+    public void MC2() {
+        System.out.println("Método MC2");
+    }
+}
