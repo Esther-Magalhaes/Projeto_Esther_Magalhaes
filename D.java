7be0ef5be70f6289ecd4632f8268ceb5641b2f25
Esther-Magalhaes <esthermags21@gmail.com>
2024-04-08 13:14:09 -0300

    Criei a classe D

---
+++ D.java
@@ -0,0 +1,28 @@
+public class D {
+    private String D1;
+    private Integer D2;
+
+    public String getD1() {
+        return D1;
+    }
+
+    public void setC1(String D1) {
+        this.D1 = D1;
+    }
+
+    public Integer getD2() {
+        return D2;
+    }
+
+    public void setD2(Integer D2) {
+        this.D2 = D2;
+    }
+
+    public void MD1() {
+        System.out.println("Método MC1");
+    }
+
+    public void MD2() {
+        System.out.println("Método MC2");
+    }
+}
